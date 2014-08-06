da4e6d6dfadae7a70309f53f0dcc4b5033a79641
Jesus Roncero <jesus@roncero.org>
2014-08-06 15:25:42 +0100

    Use templates.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "io/ioutil"
     "net/http"
+    "html/template"
 )
 
 type Page struct {
@@ -32,7 +33,7 @@ func main() {
     //fmt.Println(string(p2.Body))
     http.HandleFunc("/view/", viewHandler)
     http.HandleFunc("/edit/", editHandler)
-    http.HandleFunc("/save/", saveHandler)
+//    http.HandleFunc("/save/", saveHandler)
     http.ListenAndServe(":8081", nil)
 }
 
@@ -48,19 +49,15 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         p = &Page{Title: title}
     }
-    fmt.Fprintf(w, "<h1>Editing %s</h1>" +
-                "<form action=\"/save/%s\" method=\"POST\">" +
-                "<textarea name=\"body\">%s</textarea><br>" +
-                "<input type=\"submit\" value=\"Save\">" +
-                "</form>",
-                p.Title, p.Title, p.Body)
-
+    t, _ := template.ParseFiles("edit.html")
+    t.Execute(w,p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-}
+//func saveHandler(w http.ResponseWriter, r *http.Request) error {
+//    title := r.URL.Path[len("/save/"):]
+//    filename := title + ".txt"
+//    err := ioutil.WriteFile(filename, byte("p.Body"), 0600)
+//    if err != nil {
+//        p = &Page{Title: title}
+//    }
+//}
